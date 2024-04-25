4e0a93dfaf64233d6393cd8a0643e412d40bd6cb
MuhammadOwais28 <114926646+MuhammadOwais28@users.noreply.github.com>
2024-04-25 23:59:53 +0500

    Update app.py

--- webcode/app.py
+++ webcode/app.py
@@ -8,6 +8,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-# Run the app if this file is executed
+# Run the app on port 80
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=80)
